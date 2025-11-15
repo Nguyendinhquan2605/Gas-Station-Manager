cb852f0a975b7b20fc00094ca923e127c3861907
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-11-15 16:44:49 +0700

    Create a station

--- routes/client/indexRoute.js
+++ routes/client/indexRoute.js
@@ -4,9 +4,11 @@ import FuelType from "../../models/fuel_type.model.js";
 import Brand from "../../models/brand_model.js";
 import "../../models/index.model.js";
 import sequelize from "../../configs/db.js";
+import StationFuel from "../../models/station_fuel.model.js";
 
 const router = express.Router();
 
+// [GET] /stations/station-data
 router.get("/station-data", async (req, res) => {
   try {
     const results = await Station.findAll({
@@ -30,6 +32,12 @@ router.get("/station-data", async (req, res) => {
   }
 });
 
+// [GET] /stations
+router.get("/", async (req, res) => {
+  res.render("client/station.ejs");
+});
+
+// [GET] /stations/create-station
 router.get("/create-station", async (req, res) => {
   const brands = await Brand.findAll();
   const fuelTypes = await FuelType.findAll();
@@ -40,8 +48,34 @@ router.get("/create-station", async (req, res) => {
   });
 });
 
+// [POST] /stations/create-station
 router.post("/create-station", async (req, res) => {
-  res.send("ok");
-  console.log(req.body);
+  const fuelIds = Array.isArray(req.body.fuel_id)
+    ? req.body.fuel_id.map((id) => parseInt(id))
+    : [parseInt(req.body.fuel_id)];
+
+  const data = {
+    name: req.body.name,
+    lat: parseFloat(req.body.lat),
+    lng: parseFloat(req.body.lng),
+    address: req.body.address,
+    phone: req.body.phone,
+    hours: req.body.hours,
+    services: req.body.services,
+    brand_id: parseInt(req.body.brand_id),
+  };
+
+  const station = await Station.create(data);
+  const stationId = station.id;
+
+  await StationFuel.bulkCreate(
+    fuelIds.map((fid) => ({
+      station_id: stationId,
+      fuel_id: fid,
+    }))
+  );
+
+  res.redirect("/stations");
 });
+
 export default router;
