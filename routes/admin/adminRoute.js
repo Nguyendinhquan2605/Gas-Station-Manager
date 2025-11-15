f233b4dc9a5e6c9e4409b94ad126408226939964
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-11-16 01:01:15 +0700

    Layout edit station

--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -7,7 +7,7 @@ import StationFuel from "../../models/station_fuel.model.js";
 
 const router = express.Router();
 
-// [GET] /stations/create-station
+// [GET] /admin/create-station
 router.get("/create-station", async (req, res) => {
   const brands = await Brand.findAll();
   const fuelTypes = await FuelType.findAll();
@@ -19,7 +19,7 @@ router.get("/create-station", async (req, res) => {
   });
 });
 
-// [POST] /stations/create-station
+// [POST] /admin/create-station
 router.post("/create-station", async (req, res) => {
   const fuelIds = Array.isArray(req.body.fuel_id)
     ? req.body.fuel_id.map((id) => parseInt(id))
@@ -47,6 +47,65 @@ router.post("/create-station", async (req, res) => {
   );
 
   res.redirect("/stations");
+});
+
+// [GET] /admin/Alls-stations
+router.get("/Alls-stations", async (req, res) => {
+  const stations = await Station.findAll({
+    include: [
+      {
+        model: FuelType,
+        through: { attributes: [] }, // Không trả về station_fuel
+        attributes: ["fuel_name"],
+      },
+      {
+        model: Brand,
+        as: "brand",
+        attributes: ["name"],
+      },
+    ],
+  });
+
+  res.render("admin/page/stations.ejs", {
+    pageTitle: "Danh sách cây xăng",
+    stations: stations,
+  });
+});
+
+//[GET] /admin/stations/edit/:id
+router.get("/stations/edit/:id", async (req, res) => {
+  const stationId = req.params.id;
+
+  const station = await Station.findOne({
+    where: {
+      id: stationId,
+    },
+    include: [
+      {
+        model: FuelType,
+        through: { attributes: [] },
+        attributes: ["id", "fuel_name"],
+      },
+      {
+        model: Brand,
+        as: "brand",
+        attributes: ["name"],
+      },
+    ],
+  });
+
+  const brands = await Brand.findAll();
+  const fuelTypes = await FuelType.findAll();
+  const selectedFuelIds = station.FuelTypes.map((f) => f.id);
+
+  //   res.json(station);
+  res.render("admin/page/edit-station.ejs", {
+    pageTitle: "Chỉnh sửa cây xăng",
+    station: station,
+    brands: brands,
+    fuelTypes: fuelTypes,
+    selectedFuelIds: selectedFuelIds,
+  });
 });
 
 export default router;
